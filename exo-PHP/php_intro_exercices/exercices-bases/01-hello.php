7c6531bc65d3848fe52bdb5867e28276940205ce
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-11-16 16:26:15 +0100

    exo php

---
+++ exo-PHP/php_intro_exercices/exercices-bases/01-hello.php
@@ -0,0 +1,14 @@
+<?php
+
+function hello(string $name) :string{
+    if ($name === "") {
+        return " nobody";
+    }
+    else {
+    return "Hello $name";
+}
+}
+
+echo hello("Guillaume");
+echo hello ("");
+
---
+++ exo-PHP/php_intro_exercices/exercices-bases/02-numbers.php
@@ -0,0 +1,17 @@
+<?php 
+
+//exo 2.A
+
+function getSum(int $nb1, int $nb2) : int {
+    return $nb1+$nb2;
+}
+
+//echo getSum(5,3);
+
+//exo 2.B
+
+function getSub(int $nb3, int $nb4) : int {
+    return $nb3-$nb4;
+}
+
+echo getSub(5,3);
