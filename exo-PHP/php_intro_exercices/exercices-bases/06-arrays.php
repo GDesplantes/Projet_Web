f9f4408f3de83e21707ed10211126bd62d385add
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-11-21 15:51:41 +0100

    exo php tableau

--- exo-PHP/php_intro_exercices/exercices-bases/06-arrays.php
+++ exo-PHP/php_intro_exercices/exercices-bases/06-arrays.php
@@ -1,8 +1,41 @@
 <?php
 
-$names =['Joe','Jack','Léa','Zoé','Néo'];
+$names = ['Joe','Jack','Léa','Zoé','Néo'];
 
-function firstItem(array $tab1)
+function firstItem(array $array)
 {
+    if (empty($array))
+    {
+        return null;
+    }
 
+        return $array[0];
+    
+    
 }
+
+// echo firstItem($names);
+
+//Exo 6.B
+
+function lastItem(array $array)
+{
+    if (empty($array))
+    {
+        return null;
+    }
+
+    return $array[sizeof($array)-1];
+}
+
+// echo lastItem($names);
+
+function sortItems(array $array){
+    if(empty($array))
+    {
+        return [];
+    }
+    rsort($array) ;
+    return implode(',',$array);
+}
+echo(sortItems($names));
