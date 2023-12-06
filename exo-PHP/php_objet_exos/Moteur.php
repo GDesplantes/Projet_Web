5b4dd92ec7ba73e04d168a9165e029c3b1cdf5ac
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-12-06 14:29:28 +0100

    push

--- exo-PHP/php_objet_exos/Moteur.php
+++ exo-PHP/php_objet_exos/Moteur.php
@@ -2,13 +2,22 @@
 
 class Moteur
 {
-    private string $marque;
-    private float $vitesseMax;
+    public string $brand;
+    public float $maxSpeed;
 
-    public function __construct(string $nouvelleMarque, float $nouvelleVitesseMax)
+    public function __construct(string $brand, float $maxSpeed)
     {
-        // HYDRATATION
-        $this->marque = $nouvelleMarque;
-        $this->vitesseMax = $nouvelleVitesseMax;
+        $this->brand = $brand;
+        $this->maxSpeed = $maxSpeed;
+    }
+
+    public function getBrand() : string
+    {
+        return $this->brand;
+    }
+
+    public function getMaxSpeed() : float
+    {
+        return $this->maxSpeed;
     }
 }
--- exo-PHP/php_objet_exos/voiture.php
+++ exo-PHP/php_objet_exos/voiture.php
@@ -1,26 +1,85 @@
 <?php
 
-require('Moteur.php');
+require_once("./Moteur.php");
+
 class Voiture
 {
-    private string $modele;
-    private string $marque;
-    private int $poids;
-    private Moteur $moteur;
+    protected string $brand;
+    protected string $model;
+    protected int $weight;
+    public $maxSpeed;
+    public $motor;
 
-    public function __construct(string $nouveauModele = "pizza", string $nouvelleMarque = "Findus", int $nouveauPoids = 15000, Moteur $moteur)
+    public function __construct(string $brand, string $model, int $weight, $motor)
     {
-        $this->modele = $nouveauModele;
-        $this->marque = $nouvelleMarque;
-        $this->poids = $nouveauPoids;
-        $this->moteur = new Moteur();
-
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->weight = $weight;
+        $this->motor = $motor;
+        $this->maxSpeed = $this->getMaxSpeed();
     }
 
-  
+    public function getBrand() : string
+    {
+        return $this->brand;
+    }
 
+    public function getModel() : string
+    {
+        return $this->model;
+    }
+
+    public function getWeight() : int
+    {
+        return $this->weight;
+    }
+
+    public function getMaxSpeed() : float
+    {
+        $this->maxSpeed = $this->motor->maxSpeed - ($this->weight * 8 / 100);
+        return $this->maxSpeed;
+    }
+
+    public function getMotorBrand() : string
+    {
+        return $this->motor->brand;
+    }
+
+    public function getMotorMaxSpeed() : float
+    {
+        return $this->motor->maxSpeed;
+    }
 
 }
 
-$maVoiture = new Voiture();
-var_export($maVoiture);
+$peugeotSUV = new Voiture("Peugeot", "SUV 5008", 1500, new Moteur("Renault", 300));
+
+echo "Voiture : 5008";
+echo"<br>";
+
+echo $peugeotSUV->getBrand();
+echo"<br>";
+
+echo $peugeotSUV->getModel();
+echo"<br>";
+
+echo $peugeotSUV->getWeight() . " kg";
+echo"<br>";
+
+echo $peugeotSUV->getmaxSpeed() . " KmH";
+echo"<br>";
+echo"<br>";
+
+echo "Moteur : ";
+echo "<br>";
+
+echo $peugeotSUV->getMotorBrand();
+echo"<br>";
+
+echo $peugeotSUV->getMotormaxSpeed() . " KmH";
+echo"<br>";
+echo"<br>";
+
+var_dump($peugeotSUV);
+echo"<br>";
+echo"<br>";
